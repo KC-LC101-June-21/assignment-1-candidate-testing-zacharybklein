fc2ed48d0292c24dff0f6a6a0089fb1cfa386d80
zacharybklein <85319595+zacharybklein@users.noreply.github.com>
2021-06-10 01:32:52 +0000

    Completed part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,15 +2,14 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 // TODO 1.1a: Define candidateName // 
 
-
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = "";
 
 
 function askForName() {
@@ -20,27 +19,36 @@ let candidateName = (input.question("Please enter your name: "));
 console.log("Welcome " + candidateName + "!");
 }
 
+
+
+
+
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 function askQuestion() {
 
-candidateAnswer = (input.question(question));
+for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = (input.question(questions[i]));
+    if (candidateAnswer === correctAnswers[i]) 
+    {
+    console.log(`Good job! ${correctAnswers[i]} is right!`);
+    }    
+    else {
+    console.log(`I'm sorry, ${correctAnswers[i]} is not correct.`) 
+    } 
 }
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+}
 
 function gradeQuiz(candidateAnswers) {
-  if (candidateAnswer === correctAnswer) {
-    console.log("Correct!");
-} else {
-    console.log("Incorrect!") 
+  console.log("Thank you for your participation.")
+  let grade;
+  return grade;
 }
+
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-  let grade;
-  
 
-  return grade;
-}
+
 
 function runProgram() {
   askForName();
