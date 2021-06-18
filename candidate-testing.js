692b1ad374365d1ec89ddbe03eda14204b0a4c9d
zacharybklein <85319595+zacharybklein@users.noreply.github.com>
2021-06-18 02:06:45 +0000

    changed where correct answers stored.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,7 +5,8 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
-let correctAnswer = [];
+let correctAnswer = ['Sally Ride'];
+let numberRight = [];
 let candidateAnswer = [];
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
@@ -29,7 +30,7 @@ for (let i = 0; i < questions.length; i++) {
     console.log(`${i + 1}) ${questions[i]}`);
     candidateAnswers.push(input.question());
     if (candidateAnswers[i].toString().toLowerCase() === correctAnswers[i].toString().toLowerCase()) {
-    correctAnswer.push(candidateAnswers[i]);
+    numberRight.push(candidateAnswers[i]);
     }    
     else {
     } 
@@ -52,7 +53,7 @@ function gradeQuiz(candidateAnswers) {
     } 
   }
   let grade = (score / candidateAnswers.length) * 100;
-    console.log(`>>> Overall Grade: ${grade}% (${correctAnswer.length} of 5 responses correct) <<<`)
+    console.log(`>>> Overall Grade: ${grade}% (${numberRight.length} of 5 responses correct) <<<`)
     if (grade >= 80) {
     console.log(`>>> Status: PASSED <<<`);
   }
